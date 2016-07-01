0732340b4da0373cf9f50f42f4bbeb9cec930bb5
4iar <4iar@users.noreply.github.com>
2016-07-01 19:08:56 +0100

    Fix indent level

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import game from './gameReducer';
 
- const rootReducer = combineReducers({
-   game
- });
- 
- export default rootReducer;
+const rootReducer = combineReducers({
+  game
+});
+
+export default rootReducer;
