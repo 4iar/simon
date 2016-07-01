f22c5e05eef4ffc4fbf05f4d929517c5f018f41b
4iar <4iar@users.noreply.github.com>
2016-07-01 14:24:08 +0100

    Add control buttons and display

--- src/containers/InnerControls.js
+++ src/containers/InnerControls.js
@@ -5,7 +5,28 @@ import '../styles/inner-controls.scss';
 export default class InnerControls extends React.Component {
   render() {
     return (
-      <div className="inner-circle"></div>
+      <div className="inner-circle">
+        <p className="simon-logo">Simon</p>
+
+        <div className="middle-row">
+          <section className="count">
+            <div className="count-counter">02</div>
+            <div className="count-text">COUNT</div>
+          </section>
+
+          <section className="start">
+            <button className="button start-button"></button>
+            <div className="start-text">START</div>
+          </section>
+
+          <section className="strict">
+            <div className="strict-indicator on"></div>
+            <button className="button strict-button"></button>
+            <div className="strict-text">STRICT</div>
+          </section>
+        </div>
+
+      </div>
     );
   }
 }
