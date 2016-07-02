190388f160a9cb118f35c6efc9643ae750e07a01
4iar <4iar@users.noreply.github.com>
2016-07-02 15:07:33 +0100

    Implement start button logic

--- src/containers/StartButton.js
+++ src/containers/StartButton.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { startGame } from '../actions/gameActions';
+import { startGame, setNextButton } from '../actions/gameActions';
+import getRandomButton from '../utils/getRandomButton';
 
 import '../styles/inner-controls.scss';
 
 
-@connect(null, {startGame}, null, {withRef: true})
+@connect(null, {startGame, setNextButton}, null, {withRef: true})
 export default class StartButton extends React.Component {
+  handleClick() {
+    this.props.startGame();
+    this.props.setNextButton(getRandomButton());
+  }
+
   render() {
     return (
       <section className="start">
-        <button onClick={this.props.startGame.bind(this)} className="button start-button"></button>
+        <button onClick={this.handleClick.bind(this)} className="button start-button"></button>
         <div className="start-text">START</div>
       </section>
     );
---
+++ src/utils/getRandomButton.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+
+const getRandomButton = () => {
+  const colours = ['red', 'yellow', 'green', 'blue'];
+  return _.sample(colours);
+}
+
+export default getRandomButton;
