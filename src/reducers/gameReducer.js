013af578627349014ef48a627f3615bc3f1707af
4iar <4iar@users.noreply.github.com>
2016-07-02 15:08:24 +0100

    Minor formatting changes and remove comments

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -3,11 +3,10 @@ import initialState from './initialState';
 export default function (state = initialState, action) {
   switch (action.type) {
     case 'START_GAME':
-      return state; 
+      return state;
     case 'TOGGLE_STRICTMODE':
-      return {...state, strictMode: !state.strictMode}; 
+      return {...state, strictMode: !state.strictMode};
     case 'SET_NEXT_BUTTON':
-      console.log(state);
       return {...state,
         count: state.count + 1,
         sequence: {
