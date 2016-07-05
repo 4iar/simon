a9be6e2472a91ad6c7900b672d91f3d05d0ea4be
4iar <4iar@users.noreply.github.com>
2016-07-05 20:26:16 +0100

    Use fcc provided sounds

--- src/containers/Audio.js
+++ src/containers/Audio.js
@@ -14,6 +14,10 @@ export default class Audio extends React.Component {
   render() {
     return (
       <div>
+        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" playing={this.props.audioToPlay['red']} />
+        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound2.mp3" playing={this.props.audioToPlay['green']} />
+        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound3.mp3" playing={this.props.audioToPlay['yellow']} />
+        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound4.mp3" playing={this.props.audioToPlay['blue']} />
         <ReactPlayer url="../sounds/tone1.mp3" playing={this.props.wrong} />
       </div>
     );
