10e31267aeee447b1588c829d2e88372048f4e0d
4iar <4iar@users.noreply.github.com>
2016-07-05 21:36:15 +0100

    Use Howl to handle sounds

--- src/containers/Audio.js
+++ src/containers/Audio.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
 import { connect } from 'react-redux';
+import { Howl } from 'howler';
+
+import _ from 'lodash';
 
 const getState = (state) => {
+  const audioToPlay = [].concat(_.invert(state.game.audio, true)[ "true" ]);
   return {
-    audioToPlay: state.game.audio,
-    wrong: state.game.notifyWrong
+    audioToPlay
   };
 };
 
 @connect(getState, null, null, {withRef: true})
 export default class Audio extends React.Component {
+  constructor() {
+    super();
+    this.sounds = {
+      red: new Howl({urls:['../sounds/button1.mp3']}),
+      green: new Howl({urls:['../sounds/button2.mp3']}),
+      blue: new Howl({urls:['../sounds/button3.mp3']}),
+      yellow: new Howl({urls:['../sounds/button4.mp3']}),
+      wrong: new Howl({urls:['../sounds/wrong.mp3']})
+    };
+  }
+
   render() {
+    this.props.audioToPlay.forEach((audioID) => {
+      if (audioID){
+        this.sounds[audioID].play();
+      }
+    });
+
+    //TODO: add mute handing
     return (
       <div>
-        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" playing={this.props.audioToPlay['red']} />
-        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound2.mp3" playing={this.props.audioToPlay['green']} />
-        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound3.mp3" playing={this.props.audioToPlay['yellow']} />
-        <ReactPlayer url="https://s3.amazonaws.com/freecodecamp/simonSound4.mp3" playing={this.props.audioToPlay['blue']} />
-        <ReactPlayer url="../sounds/tone1.mp3" playing={this.props.wrong} />
       </div>
     );
   }
